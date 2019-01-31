709288646479a3dc81af1159b1e2a7b17bb8baf6
clare <kirabo646@googlemail.com>
2019-01-31 18:27:44 +0000

    added the requests so that post push an e.c.t can work

--- src/main/java/com/qa/persistence/repository/AccountDBImplementation.java
+++ src/main/java/com/qa/persistence/repository/AccountDBImplementation.java
@@ -6,6 +6,7 @@ import static javax.transaction.Transactional.TxType.SUPPORTS;
 import java.util.Collection;
 
 import javax.enterprise.inject.Default;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -21,11 +22,13 @@ public class AccountDBImplementation implements AccountRepository {
 
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
+	
+	@Inject
 	private JSONUtil jsonUtil;
 	
 	@Override
 	public String getAllAccounts() {
-	    Query query = em.createQuery("Select a from Account a");
+	    Query query = em.createQuery("SELECT a FROM Account a");
 	    Collection<Account> accounts = (Collection<Account>) query.getResultList();
 		return jsonUtil.getJSONForObject(accounts);
 	}
@@ -47,7 +50,7 @@ public class AccountDBImplementation implements AccountRepository {
 		
 		Account thisAccount = em.find(Account.class, id);
 		if(thisAccount != null) {
-			em.detach(thisAccount);
+			em.remove(thisAccount);
 			return "{ \"Message\" : \"The account has been deleted\"}";
 		}
 		
--- src/main/java/com/qa/rest/AccountEndpoint.java
+++ src/main/java/com/qa/rest/AccountEndpoint.java
@@ -11,7 +11,7 @@ import javax.ws.rs.Produces;
 
 import com.qa.buisness.AccountServices;
 
-@Path("/author")
+@Path("/account")
 public class AccountEndpoint {
 
 	@Inject 
